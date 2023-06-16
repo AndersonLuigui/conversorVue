3f7842bea5a11989a065f1674abd4de7271272e2
ANDERSON LUIGUI <alns1536@hotmail.com>
2023-06-16 20:55:38 -0300

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ConversorDeMoeda/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConversorDeMoeda from "./components/ConversorDeMoeda.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ConversorDeMoeda
+  
   }
 }
 </script>
---
+++ src/components/ConversorDeMoeda.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="conversor">
+        <h2>{{ moedaA }} para {{ moedaB }}</h2>
+
+    </div>
+
+</template>
+
+
+<script>
+export default {
+    name: "ConversorDeMoeda",
+    props: ["moedaA","moedaB"]
+};
+</script>
+
+
+
+<style scoped>
+
+</style>
